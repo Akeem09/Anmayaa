1f2d1c51146f88b389c250de8dab77f33c866db1
SHAIK AKEEM <138660764+Akeem09@users.noreply.github.com>
2026-06-08 16:06:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const flowers = [
+    "images/rose.png",
+    "images/jasmine.png",
+    "images/mogra.png",
+    "images/sunflower.png"
+];
+
+const container = document.getElementById("flower-container");
+
+for(let i = 0; i < 50; i++){
+
+    const flower = document.createElement("img");
+
+    flower.src = flowers[Math.floor(Math.random() * flowers.length)];
+
+    flower.classList.add("flower");
+
+    flower.style.left = Math.random() * 100 + "%";
+    flower.style.top = Math.random() * 100 + "%";
+
+    flower.style.width =
+        (60 + Math.random() * 60) + "px";
+
+    flower.style.animationDelay =
+        (Math.random() * 3) + "s";
+
+    container.appendChild(flower);
+}
+
+window.addEventListener("load", () => {
+
+    const music = document.getElementById("bgMusic");
+
+    music.play().catch(() => {
+        console.log("Autoplay blocked by browser");
+    });
+
+});
