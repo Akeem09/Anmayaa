2f675fa924d6613575c1c7fc218c5f873c307eaf
SHAIK AKEEM <138660764+Akeem09@users.noreply.github.com>
2026-06-08 17:48:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,30 @@ const container = document.getElementById("flower-container");
 
 for(let i = 0; i < 50; i++){
 
+    let left, top;
+
+    do{
+        left = Math.random() * 100;
+        top = Math.random() * 100;
+    }
+    while(
+        left > 30 && left < 70 &&
+        top > 30 && top < 70
+    );
+
     const flower = document.createElement("img");
 
-    flower.src = flowers[Math.floor(Math.random() * flowers.length)];
+    flower.src =
+        flowers[Math.floor(Math.random() * flowers.length)];
 
     flower.classList.add("flower");
 
-    flower.style.left = Math.random() * 100 + "%";
-    flower.style.top = Math.random() * 100 + "%";
+    flower.style.left = left + "%";
+    flower.style.top = top + "%";
 
-    flower.style.width =
-        (60 + Math.random() * 60) + "px";
+    flower.style.width = "60px";
+    flower.style.height = "60px";
+    flower.style.objectFit = "contain";
 
     flower.style.animationDelay =
         (Math.random() * 3) + "s";
